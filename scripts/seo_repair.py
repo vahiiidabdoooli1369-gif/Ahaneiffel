a1cbc37a3d225f055a4a7975fc8076a454e498b8
vahiiidabdoooli1369-gif <vahiiid.abdoooli.1369@gmail.com>
2026-09-23 10:02:06 +0330

    Correct canonical deduplication logic

--- scripts/seo_repair.py
+++ scripts/seo_repair.py
@@ -68,8 +68,9 @@ for p in ROOT.rglob("*.html"):
         tags=re.findall(r'<link\\b[^>]*\\brel=["\']canonical["\'][^>]*>',s,re.I)
         if len(tags)>1:
             first=tags[0]
-            s=re.sub(r'<link\\b[^>]*\\brel=["\']canonical["\'][^>]*>',lambda m:first,s,flags=re.I)
-            s=s.replace(first,"",s.count(first)-1)
+            pattern=r'<link\\b[^>]*\\brel=["\']canonical["\'][^>]*>'
+            s=re.sub(pattern,"",s,flags=re.I)
+            s=re.sub(r'</head>',first+'</head>',s,count=1,flags=re.I)
         elif len(tags)==0:
             s=re.sub(r'</head>',f'<link rel="canonical" href="{url(p)}"></head>',s,count=1,flags=re.I)
     k=p.as_posix()
